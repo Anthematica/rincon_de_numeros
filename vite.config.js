78f1e0cc471e0296ea8b9a02305b3d744513a2fc
anthematica2 <141876472+anthematica2@users.noreply.github.com>
2024-08-21 21:38:16 -0600

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://anthematica.github.io/rincon_de_numeros/",
+  base: "/rincon_de_numeros/",
 })
